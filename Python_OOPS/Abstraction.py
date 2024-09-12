7a91ca83ee107fd0d88bfce8acac8320bd671ee9
Anitha Anjaneyulu <142965890+anithaketha@users.noreply.github.com>
2024-09-12 21:13:35 +0530

    Update Abstraction.py

--- Python_OOPS/Abstraction.py
+++ Python_OOPS/Abstraction.py
@@ -1,14 +1,14 @@
 from abc import ABC,abstractmethod
-class Car:
+class College:
     def __init__(self,name,age):
         self.name=name
         self.age=age
     @abstractmethod
     def print_Detais(self):
         pass
-class Audi(Car):
+class Student(College):
     def print_Details(self):
         print("Name : ",self.name)
         print("Age : ",self.age)
-car1=Audi("Anitha",19)
-car1.print_Details()
+stu1=Audi("Anitha",19)
+stu1.print_Details()
