a56fa7b05c0de8a5d502043d00805c9e5ecd0ba2
anithaketha <anithaanjaneyulu2@gmail.com>
2024-09-12 21:05:49 +0530

    oops

--- Python_OOPS/Abstraction.py
+++ Python_OOPS/Abstraction.py
@@ -1,10 +1,14 @@
+from abc import ABC,abstractmethod
 class Car:
-    def __init__(self):
-        self.acc=False
-        self.brk=False
-    def start(self):
-        self.acc=True
-        self.brk=True
-        print("The car has started")
-car=Car()
-car.start()
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    @abstractmethod
+    def print_Detais(self):
+        pass
+class Audi(Car):
+    def print_Details(self):
+        print("Name : ",self.name)
+        print("Age : ",self.age)
+car1=Audi("Anitha",19)
+car1.print_Details()
